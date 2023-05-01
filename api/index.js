e74288008963748f44d135613870ab41bde9e34d
Mari Azh <Marzi.aghchay@gmail.com>
2023-05-01 21:14:37 +0100

    app.use

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
-console.log("")
+
+app.use("/",(req,res)=>{
+  console.log("hello this is from me ")
+})
 app.listen("5000", () => {
   console.log("Backend is running.");
 });
